b67cf7fbf511a8d53eaf8cee829e965f9c1f503f
taebin5824 <xoqls5824@gmail.com>
2025-05-29 00:34:30 +0900

    "feat: 멤버 컨트롤러 구현(추가필요)"

--- src/main/java/com/oopsw/clario/controller/MemberController.java
+++ src/main/java/com/oopsw/clario/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.oopsw.clario.controller;
 
+import com.oopsw.clario.config.auth.authdto.OAuthAttributes;
+import com.oopsw.clario.config.auth.authdto.SessionUser;
 import com.oopsw.clario.exception.EmailAlreadyExistsException;
 import com.oopsw.clario.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -19,6 +22,21 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(HttpSession session) {
+        String redirectAfterLogin = (String) session.getAttribute("redirectAfterLogin");
+        if (redirectAfterLogin != null) {
+            session.removeAttribute("redirectAfterLogin");
+            return "redirect:" + redirectAfterLogin;
+        }
+        return "redirect:/loginView";
+    }
+
+    @GetMapping("/template-test")
+    public String testTemplate() {            //// thymeleaf 테스트
+        System.out.println("🔥 /template-test 호출됨");
+        return "account/login";
+    }
 
     @GetMapping("/loginView")
     public String loginView() {
@@ -26,8 +44,14 @@ public class MemberController {
     }
 
     @GetMapping("/privacy")
-    public String privacy(@AuthenticationPrincipal OAuth2User user) {
+    public String privacy(@AuthenticationPrincipal OAuth2User user, HttpSession session) {
         if (user == null) return "redirect:/loginView";
+
+        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
+
+        if(memberService.existsByEmail(sessionUser.getEmail())) {
+            return "redirect:/account/modal";
+        }
         return "account/privacy";
     }
 
@@ -37,28 +61,37 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String join(@AuthenticationPrincipal OAuth2User user,
-                       @RequestParam String name,
+    public String join(@RequestParam String name,
                        @RequestParam String phonenum,
                        @RequestParam String password,
                        @RequestParam String confirmPassword,
+                       HttpSession session,
                        Model model) {
+        OAuthAttributes attributes = (OAuthAttributes) session.getAttribute("oauthAttributes");
+        if(attributes == null) {
+            return "redirect:/loginView";
+        }
+
+        String email = attributes.getEmail();
+
+        if(!password.equals(confirmPassword)){
+            model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
+            model.addAttribute("email", email);
+            return "account/join";
+        }
         try{
-            String email = user.getAttribute("email");
-
-            if(!password.equals(confirmPassword)){
-                model.addAttribute("error", "비밀번호가 일치하지 않습니다.");
-                model.addAttribute("email", user.getAttribute("email"));
-                return "account/join";
-            }
-
             if(!memberService.existsByEmail(email)){
-                memberService.saveMember(name, phonenum, password, email);
+                memberService.saveMember(email,name,phonenum,password);
             }
+
+            session.removeAttribute("oauthAttributes");
+            session.setAttribute("redirectAfterLogin", "/account/modal");
 
             return "redirect:/account/modal";
+
         }catch (EmailAlreadyExistsException e){
             model.addAttribute("errorMessage", e.getMessage());
+            model.addAttribute("email", email);
             return "account/join";
         }
     }

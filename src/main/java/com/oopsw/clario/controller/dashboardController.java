900172d146e66cd65f4f3bdf6f6b94588716a44f
hyeonaa02 <jeonglucy00@gmail.com>
2025-06-01 11:22:07 +0900

    merge : dashboardController merge

--- src/main/java/com/oopsw/clario/controller/dashboardController.java
+++ src/main/java/com/oopsw/clario/controller/dashboardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class DashboardController {
+public class dashboardController {
     @GetMapping("statistics")
     public String statisticsView() {
         return "statistics/statisticsView";
